2dded1215427c616305c41cf0137d70d16bf5c1c
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-01-16 14:08:35 +0100

    Adding parameters

--- software/elmer/samples/conduction-1/3d/runall.ps1
+++ software/elmer/samples/conduction-1/3d/runall.ps1
@@ -2,7 +2,7 @@ gmsh - domain.geo
 
 ElmerGrid 14 2 domain.msh -autoclean -merge 1.0e-05
 
-# ElmerSolver
+ElmerSolver
 
-ElmerGrid 2 2 domain -partdual -metiskway 16
-mpiexec -n 16 ElmerSolver_mpi
+# ElmerGrid 2 2 domain -partdual -metiskway 16
+# mpiexec -n 16 ElmerSolver_mpi
