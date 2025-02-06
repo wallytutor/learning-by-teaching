6561ed0b633bb749a6f75ad0d3a51a2b476d493a
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-02-06 14:29:09 +0100

    Added script

---
+++ software/elmer/samples/conduction-2/postprocess.jl
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+using CairoMakie
+using DelimitedFiles
+using DataFrames
+
+function get_column_names(filename; verbose = true, newline = nothing)
+    metadata = read(filename, String)
+    verbose && @info(metadata)
+
+    if isnothing(newline)
+        newline = Sys.iswindows() ? "\r\n" : "\n"
+    end
+
+    lines = split(metadata, newline)
+    lines = filter(l->occursin(r"^(\s+)(\d+):.", l), lines)
+    map(l->match(r"^\s+\d+:\s+(?<x>(.+))", l)["x"], lines)
+end
+
+function load_table(results; fname = "sides.dat")
+    head = get_column_names(joinpath(results, "$(fname).names"))
+    data = readdlm(joinpath(results, fname))
+    return DataFrame(data, head)
+end
+
+data1 = load_table("monophase/results")
+data2 = load_table("multiphase/results")
+
+x1 = 1000data1[:, "coordinate 1"]
+x2 = 1000data2[:, "coordinate 1"]
+
+y1 = data1[:, "temperature"]
+y2 = data2[:, "temperature"]
+
+with_theme() do
+    f = Figure()
+    ax = Axis(f[1, 1])
+
+    lines!(ax, x1, y1; color = :black, label = "Reference")
+    lines!(ax, x1, y1; color = :red,   label = "Phase change")
+
+    ax.xlabel = "Position [mm]"
+    ax.ylabel = "Temperature [K]"
+
+    ax.xticks = 0:10:100
+    # ax.yticks = 0:10:100
+
+    xlims!(ax, extrema(ax.xticks.val))
+    # ylims!(ax, extrema(ax.yticks.val))
+
+    f
+end
