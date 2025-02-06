81fe1fad301126904c9eb5dbc1e534d2b5e42138
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-02-06 15:39:34 +0100

    Now working

--- software/elmer/samples/conduction-2/postprocess.jl
+++ software/elmer/samples/conduction-2/postprocess.jl
@@ -3,7 +3,7 @@ using CairoMakie
 using DelimitedFiles
 using DataFrames
 
-function get_column_names(filename; verbose = true, newline = nothing)
+function get_column_names(filename; verbose = false, newline = nothing)
     metadata = read(filename, String)
     verbose && @info(metadata)
 
@@ -22,30 +22,32 @@ function load_table(results; fname = "sides.dat")
     return DataFrame(data, head)
 end
 
-data1 = load_table("monophase/results")
-data2 = load_table("multiphase/results")
+function workflow()
+    data1 = load_table("monophase/results")
+    data2 = load_table("multiphase/results")
 
-x1 = 1000data1[:, "coordinate 1"]
-x2 = 1000data2[:, "coordinate 1"]
+    x1 = 1000data1[:, "coordinate 1"]
+    x2 = 1000data2[:, "coordinate 1"]
 
-y1 = data1[:, "temperature"]
-y2 = data2[:, "temperature"]
+    y1 = data1[:, "temperature"]
+    y2 = data2[:, "temperature"]
 
-with_theme() do
-    f = Figure()
-    ax = Axis(f[1, 1])
+    with_theme() do
+        f = Figure()
+        ax = Axis(f[1, 1])
 
-    lines!(ax, x1, y1; color = :black, label = "Reference")
-    lines!(ax, x1, y1; color = :red,   label = "Phase change")
+        lines!(ax, x1, y1; color = :black, label = "Reference")
+        lines!(ax, x2, y2; color = :red,   label = "Phase change")
 
-    ax.xlabel = "Position [mm]"
-    ax.ylabel = "Temperature [K]"
+        ax.xlabel = "Position [mm]"
+        ax.ylabel = "Temperature [K]"
 
-    ax.xticks = 0:10:100
-    # ax.yticks = 0:10:100
+        ax.xticks = 0:10:100
+        # ax.yticks = 0:10:100
 
-    xlims!(ax, extrema(ax.xticks.val))
-    # ylims!(ax, extrema(ax.yticks.val))
+        xlims!(ax, extrema(ax.xticks.val))
+        # ylims!(ax, extrema(ax.yticks.val))
 
-    f
+        f
+    end
 end
