5a6d4d5f5da91ec68da592b4125e5d27e8d4c41f
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-02-06 13:35:43 +0100

    Adding cases

--- software/elmer/samples/conduction-2/runall.ps1
+++ software/elmer/samples/conduction-2/runall.ps1
@@ -1,9 +1,16 @@
 $name = "domain"
-$var_geo = "$name/$name.geo"
-$var_msh = "$name/$name.msh"
+$varGeo = "$name/$name.geo"
+$varMsh = "$name/$name.msh"
 
-gmsh - $var_geo
+if (Test-Path $varMsh) {
+    Write-Host "Mesh already created..."
+} else {
+    gmsh - $varGeo
+    ElmerGrid 14 2 $varMsh -autoclean -merge 1.0e-05 -out $name
+}
 
-ElmerGrid 14 2 $var_msh -autoclean -merge 1.0e-05 -out $name
+Write-Host "Running case 'monophase'..."
+cd monophase;  ElmerSolver > log.solver; cd ..
 
-# ElmerSolver
+Write-Host "Running case 'multiphase'..."
+cd multiphase; ElmerSolver > log.solver; cd ..
