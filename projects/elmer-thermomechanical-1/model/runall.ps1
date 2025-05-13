051f93d7c1c747bfedf7915f8a87e4883f614334
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-05-13 12:10:37 +0200

    Fixed initial condition

--- projects/elmer-thermomechanical-1/model/runall.ps1
+++ projects/elmer-thermomechanical-1/model/runall.ps1
@@ -2,7 +2,7 @@ gmsh - "domain/domain.geo"
 
 ElmerGrid 14 2 "domain/domain.msh" -autoclean -merge 1.0e-05 -out "domain"
 
-ElmerGrid 2 2 domain/ -partdual -metiskway 4
+ElmerGrid 2 2 domain/ -partdual -metiskway 8
 
-# mpiexec -n 4 ElmerSolver_mpi > log.solver
+# mpiexec -n 8 ElmerSolver_mpi > log.solver
 # ElmerSolver > log.solver
