8c2bd273de87d03c9d2f8f7d9eec9f4619a483cf
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-05-13 11:53:16 +0200

    New ignores and commands

--- projects/elmer-thermomechanical-1/model/runall.ps1
+++ projects/elmer-thermomechanical-1/model/runall.ps1
@@ -1,3 +1,8 @@
 gmsh - "domain/domain.geo"
+
 ElmerGrid 14 2 "domain/domain.msh" -autoclean -merge 1.0e-05 -out "domain"
-ElmerSolver > log.solver
+
+ElmerGrid 2 2 domain/ -partdual -metiskway 4
+
+# mpiexec -n 4 ElmerSolver_mpi > log.solver
+# ElmerSolver > log.solver
