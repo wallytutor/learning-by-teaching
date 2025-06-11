9aa95589828d486ac8d03177894eb1e58b464718
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-06-11 16:20:34 +0200

    Working on bed solution and meshing

--- projects/kiln-model/test/test.f90
+++ projects/kiln-model/test/test.f90
@@ -1,7 +1,8 @@
 program check
     use test_template,  only: run_test_template  => test
-    use test_thermo,    only: run_test_thermo    => test
     use test_numerical, only: run_test_numerical => test
+    use test_thermo,    only: run_test_thermo    => test
+    use test_kramers,   only: run_test_kramers   => test
 
     !============
     implicit none
@@ -10,13 +11,8 @@ program check
     print *, 'STARTING TESTS'
 
     call run_test_template()
-    call run_test_thermo()
     call run_test_numerical()
-    call testdev()
-
-contains
-
-    subroutine testdev()
-    end subroutine
+    call run_test_thermo()
+    call run_test_kramers()
 
 end program check
