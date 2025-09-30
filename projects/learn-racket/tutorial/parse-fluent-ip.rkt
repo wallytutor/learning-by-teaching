0d57dbb5539b47e639dc94155bd78573a789695a
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-09-30 07:48:01 +0200

    WIP

---
+++ projects/learn-racket/tutorial/parse-fluent-ip.rkt
@@ -0,0 +1,54 @@
+#lang racket
+
+(define (make-ip-hash data-version dimensions num-points)
+    (hash 'version    data-version
+          'dimensions dimensions
+          'points     num-points))
+
+(define (print-head h)
+  (printf "~a = ~a\n" 'version    (hash-ref h 'version))
+  (printf "~a = ~a\n" 'dimensions (hash-ref h 'dimensions))
+  (printf "~a = ~a\n" 'points     (hash-ref h 'points)))
+
+(define (parse-number fp)
+  (string->number (string-trim (read-line fp))))
+
+(define (read-interpolation-file file-path)
+  (call-with-input-file file-path
+    (lambda (fp)
+      (define data-version (read-line fp))
+      (define dimensions   (parse-number fp))
+      (define num-points   (parse-number fp))
+      (make-ip-hash data-version dimensions num-points))))
+
+;; (define h (make-ip-hash "3" 2 5))
+(define h (read-interpolation-file "sandbox-sample.ip"))
+(print-head h)
+
+
+; (define (read-interpolation-file path)
+;  (call-with-input-file path
+;   (lambda (in)
+;    (define version (read-line in))
+;    (define dimension (string->number (read-line in)))
+;    (define num-points (string->number (read-line in)))
+;    (define num-fields (string->number (read-line in)))
+;    (define field-names (string-split (read-line in)))
+
+;    ;; Read coordinates
+;    (define coords
+;     (for/list ([i (in-range num-points)])
+;      (map string->number (string-split (read-line in)))))
+
+;    ;; Read field values
+;    (define field-values
+;     (for/list ([f (in-list field-names)])
+;      (for/list ([i (in-range num-points)])
+;       (string->number (read-line in)))))
+
+;    ;; Return structured data
+;    (hash 'version version
+;       'dimension dimension
+;       'points coords
+;       'fields (map cons field-names field-values)))))
+
