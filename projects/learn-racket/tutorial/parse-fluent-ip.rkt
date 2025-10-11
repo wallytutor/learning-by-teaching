1e51f02042ef6ee72ce90aa24eb25c014641e75b
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-10-11 06:35:59 +0200

    WIP

--- projects/learn-racket/tutorial/parse-fluent-ip.rkt
+++ projects/learn-racket/tutorial/parse-fluent-ip.rkt
@@ -1,17 +1,57 @@
 #lang racket
 
-(define-syntax-rule (show-hash h key)
-  (printf "~a = ~a\n" 'key (hash-ref h 'key)))
+(require racket/format)
 
-(define (make-ip-hash data-version dimensions num-points)
-    (hash 'version    data-version
-          'dimensions dimensions
-          'points     num-points))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PRINTING HELPERS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (print-head h)
-  (hash? h) ((show-hash h version)
-             (show-hash h dimensions)
-             (show-hash h points)))
+(define (make-field-name name n)
+  (~a name #:min-width n #:align 'left))
+
+(define-syntax-rule (show-hash width h key)
+  (printf "~a = ~a\n" (make-field-name (symbol->string key) width) (hash-ref h key)))
+
+(define (print-spacer #:n [n 30]) (printf "~a\n" (make-string n #\-)))
+
+(define (print-label name) (printf "\n~a\n" name) (print-spacer))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; DATA STRUCTURE
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (make-ip-hash data-version num-dims num-points num-vars vars-names)
+    (hash 'data-version data-version
+          'num-dims     num-dims
+          'num-points   num-points
+          'num-vars     num-vars
+          'vars-names   vars-names))
+
+(define (print-ip-head h)
+  (when (hash? h)
+    (print-label "Interpolation file info")
+    (show-hash 15 h 'data-version)
+    (show-hash 15 h 'num-dims)
+    (show-hash 15 h 'num-points)
+    (show-hash 15 h 'num-vars)))
+
+(define (print-ip-fields h)
+  (when (hash? h)
+    (print-label "Variable names")
+    (for ([(name idx) (in-indexed (hash-ref h 'vars-names))])
+      (printf "field ~a = ~a\n" (make-field-name (number->string idx) 9) name))
+    ))
+
+(define (print-ip h)
+  (when (hash? h)
+    (print-ip-head h)
+    (print-ip-fields h)
+    (print-spacer)
+    ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PARSING
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (parse-number fp)
   (string->number (string-trim (read-line fp))))
@@ -20,31 +60,28 @@
   (call-with-input-file file-path
     (lambda (fp)
       (define data-version (read-line fp))
-      (define dimensions   (parse-number fp))
+      (define num-dims     (parse-number fp))
       (define num-points   (parse-number fp))
       (define num-vars     (parse-number fp))
-      (printf "~a \n" num-vars)
 
-      ; (define field-names
-      ;   (for/list ([i (in-range num-vars)])
-      ;     (read-line fp)))
-      ; (printf "~a \n" field-names)
+      (define vars-names
+        (for/list ([_ (in-range num-vars)])
+          (read-line fp)))
 
-      (make-ip-hash data-version dimensions num-points))))
+      (make-ip-hash
+        data-version
+        num-dims
+        num-points
+        num-vars
+        vars-names
+        ))))
 
-;; (define h (make-ip-hash "3" 2 5))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; APPLICATION
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define h (read-interpolation-file "sandbox-sample.ip"))
-(print-head h)
-
-
-; (define (read-interpolation-file path)
-;  (call-with-input-file path
-;   (lambda (in)
-;    (define version (read-line in))
-;    (define dimension (string->number (read-line in)))
-;    (define num-points (string->number (read-line in)))
-;    (define num-fields (string->number (read-line in)))
-;    (define field-names (string-split (read-line in)))
+(print-ip h)
 
 ;    ;; Read coordinates
 ;    (define coords
