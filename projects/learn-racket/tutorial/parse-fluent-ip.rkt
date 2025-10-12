d8b8a73dfd454caa949e9eb91c5ff5c20e39e0ef
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-10-12 19:37:19 +0200

    Finished mini project

--- projects/learn-racket/tutorial/parse-fluent-ip.rkt
+++ projects/learn-racket/tutorial/parse-fluent-ip.rkt
@@ -20,12 +20,23 @@
 ;; DATA STRUCTURE
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (make-ip-hash data-version num-dims num-points num-vars vars-names)
-    (hash 'data-version data-version
-          'num-dims     num-dims
-          'num-points   num-points
-          'num-vars     num-vars
-          'vars-names   vars-names))
+(define (make-ip-hash
+  data-version
+  num-dims
+  num-points
+  num-vars
+  vars-names
+  dims
+  vars)
+  (hash 'data-version data-version
+        'num-dims     num-dims
+        'num-points   num-points
+        'num-vars     num-vars
+        'vars-names   vars-names
+        'dims         dims
+        'vars         vars))
+
+(define (printed-size arr) (string-append "#" (number->string (length arr))))
 
 (define (print-ip-head h)
   (when (hash? h)
@@ -42,10 +53,30 @@
       (printf "field ~a = ~a\n" (make-field-name (number->string idx) 9) name))
     ))
 
+(define (print-ip-dims h)
+  (when (hash? h)
+    (print-label "Coordinates")
+    (for ([(key val) (in-hash h)])
+      (printf "~a = ~a\n" (make-field-name key 15) (printed-size val)))
+    ))
+
+(define (print-ip-vars h)
+  (when (hash? h)
+    (define names (hash-ref h 'vars-names))
+    (define ip-vars (hash-ref h 'vars))
+    (print-label "Variables")
+    (for ([key names])
+      (define name (make-field-name key 15))
+      (define value (printed-size (hash-ref ip-vars key)))
+      (printf "~a = ~a\n" name value))
+    ))
+
 (define (print-ip h)
   (when (hash? h)
     (print-ip-head h)
     (print-ip-fields h)
+    (print-ip-vars h)
+    (print-ip-dims (hash-ref h 'dims))
     (print-spacer)
     ))
 
@@ -53,20 +84,40 @@
 ;; PARSING
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (parse-number fp)
-  (string->number (string-trim (read-line fp))))
+(define (read-trim fp)
+  (regexp-replace* #rx"[\r\n]" (read-line fp) ""))
+
+(define (read-number fp)
+  (string->number (read-trim fp)))
+
+(define (read-list fp n)
+  (for/list ([_ (in-range n)]) (read-trim fp)))
+
+(define (parse-field-data fp n)
+  (read (open-input-string (string-join (read-list fp n) " "))))
+
+(define (read-field-as-hash fp names num-points)
+    (define field-hash (make-hash))
+    (for ([curr-name (in-list names)])
+      (define curr-vals (parse-field-data fp (+ 1 num-points)))
+      (hash-set! field-hash curr-name curr-vals))
+    field-hash)
 
 (define (read-interpolation-file file-path)
   (call-with-input-file file-path
     (lambda (fp)
-      (define data-version (read-line fp))
-      (define num-dims     (parse-number fp))
-      (define num-points   (parse-number fp))
-      (define num-vars     (parse-number fp))
+      (define data-version (read-trim   fp))
+      (define num-dims     (read-number fp))
+      (define num-points   (read-number fp))
+      (define num-vars     (read-number fp))
 
       (define vars-names
         (for/list ([_ (in-range num-vars)])
-          (read-line fp)))
+          (read-trim fp)))
+
+      (define dims-names
+        (for/list ([i (in-range num-dims)])
+          (string-append "coordinate-" (number->string i))))
 
       (make-ip-hash
         data-version
@@ -74,28 +125,23 @@
         num-points
         num-vars
         vars-names
-        ))))
+        (read-field-as-hash fp dims-names num-points)
+        (read-field-as-hash fp vars-names num-points)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; APPLICATION
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define h (read-interpolation-file "sandbox-sample.ip"))
-(print-ip h)
+(define (main)
+  (define time-start (current-inexact-milliseconds))
+  (define h (read-interpolation-file "sample-parse-fluent.ip"))
+  (print-ip h)
+  (define time-end (current-inexact-milliseconds))
+  (printf "Elapsed time: ~a s\n" (/ (- time-end time-start) 1000))
+)
 
-;    ;; Read coordinates
-;    (define coords
-;     (for/list ([i (in-range num-points)])
-;      (map string->number (string-split (read-line in)))))
+(main)
 
-;    ;; Read field values
-;    (define field-values
-;     (for/list ([f (in-list field-names)])
-;      (for/list ([i (in-range num-points)])
-;       (string->number (read-line in)))))
-
-;    ;; Return structured data
-;    (hash 'version version
-;       'dimension dimension
-;       'points coords
-;       'fields (map cons field-names field-values)))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; EOF
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
