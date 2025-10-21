4d273ddc2a8e6b25687d0232534db1195bd4cb72
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-10-21 07:48:33 +0200

    WIP

--- projects/diffusion-in-solids/devel/Constants.hs
+++ projects/diffusion-in-solids/devel/Constants.hs
@@ -10,9 +10,29 @@ Provides physical constants for diffusion calculations.
 -}
 
 module Constants (
-        rGas
+        rGas,
+        molarMassCC,
+        molarMassNN,
+        molarMassFe
     ) where
+
+---------------------------------------------------------------------------------------------------
+-- General physical constants.
+---------------------------------------------------------------------------------------------------
 
 -- Ideal gas constant [J/(mol.K)].
 rGas :: Double
 rGas = 8.314462618
+
+---------------------------------------------------------------------------------------------------
+-- Atomic/molar masses [g/mol].
+---------------------------------------------------------------------------------------------------
+
+molarMassCC :: Double
+molarMassCC = 12.0
+
+molarMassNN :: Double
+molarMassNN = 14.0
+
+molarMassFe :: Double
+molarMassFe = 55.85
--- projects/diffusion-in-solids/devel/diffusion.hs
+++ projects/diffusion-in-solids/devel/diffusion.hs
@@ -1,6 +1,20 @@
 -- diffusion.hs
 
 import Carbonitriding (diffusionCoefficients)
-
+import Constants (molarMassCC, molarMassNN, molarMassFe)
 
 (dc, dn) = diffusionCoefficients 0.010 0.005 1173.15
+
+
+molarMasses = [molarMassCC, molarMassNN, molarMassFe]
+
+massFractions yc yn = [yc, yn, 1.0 - yc - yn]
+moleFractions xc xn = [xc, xn, 1.0 - xc - xn]
+
+meanMolarMassY y = 1.0 / (sum $ zipWith (/) y molarMasses)
+meanMolarMassX x = sum $ zipWith (*) x molarMasses
+
+massToMoleFraction yc yn = (xc, xn)
+    where mw = meanMolarMassY (massFractions yc yn)
+          xc = (yc / molarMassCC) * mw
+          xn = (yn / molarMassNN) * mw
