25cc29ad36d66d2d25d4635c8d4a290cef3619aa
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-10-28 20:41:59 +0100

    WIP

---
+++ projects/diffusion-in-solids/devel/Numerical.hs
@@ -0,0 +1,19 @@
+{-|
+Module      : Numerical
+Copyright   : (c) Walter Dal'Maz Silva, 2025
+License     : MIT
+Maintainer  : walter.dalmazsilva.manager@gmail.com
+Stability   : experimental
+Portability : unknown, intended portable
+
+Provides numerical methods used in diffusion calculations.
+-}
+module Numerical (
+
+    ) where
+
+
+
+---------------------------------------------------------------------------------------------------
+-- EOF
+---------------------------------------------------------------------------------------------------
